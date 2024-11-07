64047e7387153af317ad5e0bb9e7546da04ba60b
Deansie <dennis.andersen@lerniastudent.se>
2024-11-07 22:16:46 +0100

    Added animation to mobile menu

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.querySelector(".header__nav__button");
     const mobileNav = document.querySelector(".mobile__nav");
     const mobileNavClose = document.querySelector(".mobile__nav__close");
+    const container = document.querySelector(".container");
 
-    menuButton.addEventListener("click", function () {
-        mobileNav.classList.add("openMenu");
+    menuButton.addEventListener("click", function (delayedOpenMenu) {
+        container.classList.add("fade-out");
+        
+        function delayedOpenMenu () {
+            mobileNav.classList.add("openMenu");
+        };
+        
+        setTimeout(delayedOpenMenu, 200);
+        
     });
 
     mobileNavClose.addEventListener("click", function () {
+        container.classList.remove("fade-out");
         mobileNav.classList.remove("openMenu");
     });
 
